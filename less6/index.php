7d9f14985e516309c17a715ca5b3d13cea06c2c9
antinio <shramko.ag@gmail.com>
2016-10-16 16:30:07 +0300

    Add function readDirectoryFiles

--- less6/index.php
+++ less6/index.php
@@ -1,2 +1,14 @@
 <?php
 
+
+function readDirectoryFiles()
+{
+	$dir = "img/";
+	$files = array_diff(scandir($dir), array('.', '..'));
+	echo "Файлы папки $dir" . "<br>";
+	foreach ($files as $i => $filename) {
+		echo $filename . "<br>";
+	}
+}
+
+echo readDirectoryFiles();
